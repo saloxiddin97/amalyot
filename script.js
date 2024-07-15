a02dcdb8417a05b8cd2e37b369e654aa7a9f02ef
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-07-15 14:37:41 +0500

    amalyot

---
+++ script.js
@@ -0,0 +1,7 @@
+let h1=document.querySelector("h1")
+let a=prompt("text rangni kriting")
+h1.style.color=a
+
+let rang=prompt("body orqa fon kriting")
+let body=document.querySelector("body")
+   body.style.background=rang
